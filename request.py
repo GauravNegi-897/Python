90a02daf27feada212f028e88e229de74c0fb942
Gaurav Negi <gnegi897@gmail.com>
2026-01-20 20:24:34 +0530

    Add initial Python scripts for basic functionality

---
+++ request.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.get("http://api.github.com");
+
+print(response)
---
+++ variables.py
@@ -0,0 +1,25 @@
+ 
+name = "my name is gaurav negi"
+
+print(name)
+
+my_long_name = """
+My Name is gaurav Negi 
+I am a software engineer
+"""
+print(my_long_name)
+
+
+first_name = "Gaurav"
+last_name = "Negi"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+
+dash = "-"
+
+long_dash = dash * 100
+
+print(long_dash)
